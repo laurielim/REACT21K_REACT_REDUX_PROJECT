a31db95bad3cc0ef592c7862a9a6c54794526776
laurielim <laurie.limsam@gmail.com>
2021-09-14 10:50:33 +0300

    Generate preview items

--- src/containers/ProductsPreviewContainer.js
+++ src/containers/ProductsPreviewContainer.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import ProductCard from "../components/ProductCard";
 
 const ProductsPreview = () => {
+	const rocks = useSelector((state) => state.rocks);
+
+	const rockSample = rocks.sort(() => 0.5 - Math.random()).slice(0, 4);
 	return (
 		<section>
 			<h3>Our Products</h3>
-			{/* <ProductCard /> */}
+			<ul>
+				{rockSample.map((rock) => (
+					<li>
+						<ProductCard key={rock.id} rock={rock} />
+					</li>
+				))}
+			</ul>
 		</section>
 	);
 };
